48584830d47c125537543de40e8d6626493b8312
nadazaric <nada0zaric@gmail.com>
2022-04-15 20:15:21 +0200

    Added base user class.

---
+++ Klinika/Roles/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Klinika.Roles
+{
+    internal class User
+    {
+        #region [ --- VARIABLES --- ]
+        string Email { get; set; }
+        string Password { get; set; }
+        #endregion
+
+        #region [ --- CONSTRUCTORS --- ]
+        public User (string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+        #endregion
+    }
+}
