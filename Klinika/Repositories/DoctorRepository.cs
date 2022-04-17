c6fb35cc5f216486f2a9297691325ad159e3158e
Strahinja Sekulic <strahinja0123@gmail.com>
2022-04-17 09:55:15 +0200

    Added Doctor and DoctorRepository class

---
+++ Klinika/Repositories/DoctorRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Klinika.Roles;
+
+namespace Klinika.Repositories
+{
+    internal class DoctorRepository
+    {
+        #region [ --- VARIABLES --- ]
+        public List<Doctor> Doctors { get; private set; }
+        #endregion
+
+        #region [ --- SINGLETON --- ]
+        private DoctorRepository? Instance;
+        private DoctorRepository()
+        {
+            Doctors = new List<Doctor>();
+            // TODO Load data
+        }
+        public DoctorRepository GetInstance()
+        {
+            if(Instance == null) Instance = new DoctorRepository();
+            return Instance;
+        }
+        #endregion
+        
+    }
+}
---
+++ Klinika/Roles/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.Roles
+{
+    internal class Doctor : User
+    {
+        #region [ --- VARIABLES --- ]
+        #endregion
+
+        #region [ --- CONSTRUCTORS --- ]
+        public Doctor(string email, string password) : base(email, password)
+        {
+        }
+        #endregion
+        
+    }
+}
