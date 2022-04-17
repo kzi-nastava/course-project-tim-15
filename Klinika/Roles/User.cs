7ea70931e49a1f5127aee88fe5093c82d9cfd16c
nadazaric <nada0zaric@gmail.com>
2022-04-17 09:53:55 +0200

    Added RoleType and class Patient

--- Klinika/Roles/User.cs
+++ Klinika/Roles/User.cs
@@ -2,16 +2,25 @@
 {
     abstract class User
     {
+        #region [ --- CONSTANTS --- ]
+        public enum RoleType
+        {
+            DOCTOR,
+            PATIENT
+        }
+        #endregion
         #region [ --- VARIABLES --- ]
         string Email { get; set; }
         string Password { get; set; }
+        RoleType Role { get; set; }
         #endregion
 
         #region [ --- CONSTRUCTORS --- ]
-        public User (string email, string password)
+        public User (string email, string password, RoleType role)
         {
             Email = email;
             Password = password;
+            Role = role;
         }
         #endregion
     }
