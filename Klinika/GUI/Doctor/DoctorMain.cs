7b3f99b32e50a80aa8f1ef67eb396ebebb152e11
Strahinja Sekulic <strahinja0123@gmail.com>
2022-04-27 18:19:27 +0200

    Added table view for all appointments.

--- Klinika/GUI/Doctor/DoctorMain.cs
+++ Klinika/GUI/Doctor/DoctorMain.cs
@@ -1,4 +1,5 @@
-﻿using Klinika.Roles;
+﻿using Klinika.Repositories;
+using Klinika.Roles;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,15 +14,27 @@ namespace Klinika.GUI.Doctor
 {
     public partial class DoctorMain : Form
     {
+        private User Doctor { get; set; }
         public DoctorMain(User doctor)
         {
             InitializeComponent();
-            
+            Doctor = doctor;
         }
 
         private void DoctorMainLoad(object sender, EventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("LOAD");
+            DataTable allAppointments = AppointmentRepository.GetAll(Doctor.ID, User.RoleType.DOCTOR);
+            if (AllAppointmentsTable != null)
+            {
+                allAppointments.Columns.Add("test");
+                foreach(DataRow row in allAppointments.Rows)
+                {
+                    row["test"] = "test";
+                }
+                AllAppointmentsTable.DataSource = allAppointments;
+                AllAppointmentsTable.ClearSelection();
+            }
 
         }
     }
