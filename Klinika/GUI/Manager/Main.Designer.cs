94a145599dd2dd40bb49bee88519837b1077678e
Marko Milijanovic <markomilijanovic01@gmail.com>
2022-04-30 12:33:03 +0200

    Small design changes

--- Klinika/GUI/Manager/Main.Designer.cs
+++ Klinika/GUI/Manager/Main.Designer.cs
@@ -145,6 +145,7 @@
             // 
             // equipmentComboBox
             // 
+            this.equipmentComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.equipmentComboBox.FormattingEnabled = true;
             this.equipmentComboBox.Location = new System.Drawing.Point(474, 304);
             this.equipmentComboBox.Name = "equipmentComboBox";
@@ -154,6 +155,7 @@
             // 
             // roomComboBox
             // 
+            this.roomComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.roomComboBox.FormattingEnabled = true;
             this.roomComboBox.Location = new System.Drawing.Point(199, 304);
             this.roomComboBox.Name = "roomComboBox";
@@ -163,6 +165,7 @@
             // 
             // quantityComboBox
             // 
+            this.quantityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.quantityComboBox.FormattingEnabled = true;
             this.quantityComboBox.Location = new System.Drawing.Point(615, 304);
             this.quantityComboBox.Name = "quantityComboBox";
