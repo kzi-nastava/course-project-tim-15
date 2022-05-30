e351b70bf9a1218d2adf913e1c3f27acb5dc85e8
Strahinja Sekulic <strahinja0123@gmail.com>
2022-05-30 22:11:48 +0200

    Added VacationRequest model.

---
+++ Klinika/Models/VacationRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.Models
+{
+    public class VacationRequest
+    {
+        public enum Statuses { WAITING = 'W', ACCEPTED = 'A', DENIED = 'D' }
+        public int ID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string Reason { get; set; }
+        public char Status { get; set; }
+        public bool Emergency { get; set; }
+        public string DenyReason { get; set; }
+
+        public VacationRequest(int id, int doctorID, DateTime fromDate, DateTime toDate,
+            string reason, char status, bool emergency, string denyReason)
+        {
+            ID = id;
+            DoctorID = doctorID;
+            FromDate = fromDate;
+            ToDate = toDate;
+            Reason = reason;
+            Status = status;
+            Emergency = emergency;
+            DenyReason = denyReason;
+        }
+    }
+}
