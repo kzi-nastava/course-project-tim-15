6a46aa43437102cc37ebc6e83cedf336aa35aa03
Strahinja Sekulic <strahinja0123@gmail.com>
2022-05-31 21:46:18 +0200

    Added VacationRequestService.
    - Added GetAll method.

---
+++ Klinika/Services/VacationRequestService.cs
@@ -0,0 +1,18 @@
+﻿using Klinika.Models;
+using Klinika.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.Services
+{
+    public class VacationRequestService
+    {
+        public static List<VacationRequest> GetAll(int doctorID)
+        {
+            return VacationRequestRepository.GetAll(doctorID);
+        }
+    }
+}
