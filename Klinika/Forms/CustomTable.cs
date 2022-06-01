790d1e7ed7e4097c32552668cb2c4069f32193ac
Strahinja Sekulic <strahinja0123@gmail.com>
2022-06-01 20:03:36 +0200

    Added abstract base class for custom table.

---
+++ Klinika/Forms/CustomTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klinika.Forms
+{
+    internal abstract class CustomTable<T> : DataGridView
+    {
+        private List<T> items;
+        protected string[] header;
+        public CustomTable(string[] header) : base()
+        {
+            this.header = header;
+            items = new List<T>();
+            CreateTable();
+        }
+        private void CreateTable()
+        {
+            DataTable data = new DataTable();
+            foreach (string column in header) data.Columns.Add(column);
+            DataSource = data;
+        }
+        public int GetSelectedID()
+        {
+            return Convert.ToInt32(SelectedRows[0].Cells["ID"].Value);
+        }
+        public int DeleteSelected()
+        {
+            var id = GetSelectedID();
+            Rows.RemoveAt(CurrentRow.Index);
+            return id;
+        }
+        abstract protected void Insert(T item);
+        abstract public void ModifySelected(T item);
+        abstract public T GetSelected();
+    }
+}
