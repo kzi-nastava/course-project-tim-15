9a6a4fc4af79bc2c1bc720de21507bf12984af12
nadazaric <nada0zaric@gmail.com>
2022-06-04 18:16:30 +0200

    Added new custom table for Questions
    Now data of questions will be showed in this custom table.

---
+++ Klinika/Forms/QuestionsDataGridView.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using Klinika.Models;
+
+namespace Klinika.Forms
+{
+    internal class QuestionsDataGridView : DataGridView
+    {
+        public void Fill(List<Question> questions)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("ID");
+            dataTable.Columns.Add("Question");
+            dataTable.Columns.Add("Grade");
+            DataSource = dataTable;
+            Columns["ID"].Width = 25;
+            Columns["Grade"].Width = 70;
+            if (questions == null) return;
+            foreach (Question question in questions) Insert(question);
+            ClearSelection();
+        }
+        private void Insert(Question question)
+        {
+            DataTable? dt = DataSource as DataTable;
+            DataRow newRow = dt.NewRow();
+            newRow["ID"] = question.id;
+            newRow["Question"] = question.context;
+            newRow["Grade"] = 1;
+            dt.Rows.Add(newRow);
+        }
+        public List<Answer> GetAll()
+        {
+            var answers = new List<Answer>();
+            foreach (DataGridViewRow row in Rows)
+            {
+                int questionID = Convert.ToInt32(row.Cells[0].Value);
+                int grade = Convert.ToInt32(row.Cells[2].Value);
+                answers.Add(new Answer(questionID, grade));
+            }
+            return answers;
+        }
+        public void SetGrade (int grade)
+        {
+            SelectedRows[0].Cells["Grade"].Value = grade;
+        }
+    }
+}
